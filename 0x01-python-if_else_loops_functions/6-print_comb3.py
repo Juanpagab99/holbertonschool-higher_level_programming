19c13f2f3bca6c742407761fef09211ec38ac220
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-27 23:33:38 +0000

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        if i != j:
+            if j == 0 and i == 1:
+                continue
+            print("{}{}".format(i, j), end=', ')
+print("{}{}".format(i + 1, j))
