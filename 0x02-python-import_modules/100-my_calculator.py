90abda75e83549a88611f4a605cc8a97a9371cbe
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-28 23:42:23 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == "+":
+    elif argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
