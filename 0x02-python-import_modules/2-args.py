5842cb87983c52cd6ef4ac401b4a10e2ecd3114a
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-28 21:06:05 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from sys import argv
-	count = 0
-	argu = len(argv) - 1
-	if argu == 0:
-		print("{} arguments.".format(argu))
-	elif argu == 1:
-		print("{} argument:".format(argu))
-		for j in argv:
-			if count:
-				print("{} {}".format(count, j))
-			count += 1
-	elif argu > 1:
-		print("{} arguments:".format(argu))
-		for j in argv:
-			if count:
-				print("{} {}".format(count, j))
-			count += 1
+    from sys import argv
+    count = 0
+    argu = len(argv) - 1
+    if argu == 0:
+        print("{} arguments.".format(argu))
+    elif argu == 1:
+        print("{} argument:".format(argu))
+        for j in argv:
+            if count:
+                print("{} {}".format(count, j))
+            count += 1
+    elif argu > 1:
+        print("{} arguments:".format(argu))
+        for j in argv:
+            if count:
+                print("{} {}".format(count, j))
+            count += 1
