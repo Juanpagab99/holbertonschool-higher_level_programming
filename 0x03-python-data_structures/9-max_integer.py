65951cdca2cdfadff775208d50e9e9d3f78ce8fb
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-05 21:32:57 +0000

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_val = my_list[0]
-    length = len(my_list)
     if my my_list:
+        max_val = my_list[0]
+        length = len(my_list)
         for i in range(0, length - 1):
             if max_val < my_list[i]:
                 max_val = my_list[i]
