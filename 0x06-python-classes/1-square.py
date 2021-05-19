696768bb97bb345131538ccecd591e50f21a8d64
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-19 01:07:39 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Class Square'''
+class Square():
+    '''Defines a square.
+    Attributes:
+    sizes: siza of a square
+    def __init__(self, size):
+        self._size = size'''
+    def __init__(self, size):
+        self.__size = size
