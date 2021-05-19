eea8a63ff116b9d6cbb4276fd781cb01f43d9e9d
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-19 03:28:11 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,5 +17,6 @@ class Square():
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    '''method to return area of square'''
     def area(self):
         return self.__size * self.__size
