1dc27709c6fbf7ce79cafc7675865d83ecd7e8df
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-20 18:30:53 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,17 +3,16 @@
 
 
 class Square():
-    '''
-	Defines a square.
+    """
+    Defines a square.
     Attributes:
-    sizes: siza of a square
-	'''
+    size: size of a square
+    """
 
-	'''
-	Method to init a varible size
-	'''
-	
-	def __init__(self, size=0):
+    """ Method to initializate
+    a varible called size """
+
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -21,8 +20,8 @@ class Square():
         else:
             self.__size = size
     '''
-	method to return area of square
-	'''
+    method to return area of square
+    '''
 
     def area(self):
         return self.__size * self.__size
