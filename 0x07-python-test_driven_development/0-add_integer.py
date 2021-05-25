6d2756d6469cf5f3b208a1161ae63abaac48ce1d
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-25 20:13:26 +0000

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This function returns
+    add of two numbers"""
+def add_integer(a, b=98):
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
