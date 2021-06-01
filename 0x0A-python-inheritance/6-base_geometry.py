78c06bc8e93d1a4987ba825f8e3770d1f0de6d53
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-01 23:51:23 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """ Public instance and
+        raise an exception """
+    def area(self):
+        raise Exception("area() is not implemented")
