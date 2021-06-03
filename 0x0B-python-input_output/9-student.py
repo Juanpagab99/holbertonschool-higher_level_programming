afcdb1c2fa25ba97def943ed82a0bb2e838df5a9
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-03 01:21:04 +0000

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,13 @@
 
 
 class Student:
-	""" Information about students """
-	def __init__(self, first_name, last_name, age):
-		""" Initializate """
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+    """ Information about students """
+    def __init__(self, first_name, last_name, age):
+        """ Initializate """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	def to_json(self):
-		"""Directory"""
-		return self.__dict__
+    def to_json(self):
+        """Directory"""
+        return self.__dict__
