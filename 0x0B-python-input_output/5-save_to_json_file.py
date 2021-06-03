074a164b8cbf519f0433d5340cca738e0b5ec62b
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-03 00:07:29 +0000

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-	""" JSON representation """
-	with open(filename, 'w') as f:
-		json.dumps(my_obj, f)
+    """ JSON representation """
+    with open(filename, 'w') as f:
+        json.dumps(my_obj, f)
