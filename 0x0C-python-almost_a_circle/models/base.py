fa4104f6e50792d724cb812ce54a5e2ec7fbcbf2
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-08 21:42:44 +0000

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 the Base Class """
 
 
+import json
+
+
 class Base:
     """ Class that initializate
     an object """
@@ -16,3 +19,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ JSON string
+        representation """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+        
