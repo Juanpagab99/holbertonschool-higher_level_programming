080a14922099e6004570552e776e3bcd9dceb4dc
Juanpagab99 <juanpagab99@gmail.com>
2021-08-03 04:35:48 +0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const tam = process.argv.length;
-if (tam < 3) {
-	console.log('No argument');
+if (tam === 2) {
+  console.log('No argument');
 } else if (tam === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
