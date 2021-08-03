1c5039ba27bc77091e0a8fb22cd8a035d36a9f43
Juanpagab99 <juanpagab99@gmail.com>
2021-08-04 06:28:53 +0700

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const empty_array = [];
+  const emptyarray = [];
   const x = list.length - 1;
   for (let i = x; i >= 0; --i) {
-    empty_array.push(list[i]);
+    emptyarray.push(list[i]);
   }
-  return empty_array;
+  return emptyarray;
 };
