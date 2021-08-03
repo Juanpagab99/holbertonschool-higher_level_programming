e759751a798b8ecaad107070f945938d2d9a0a83
Juanpagab99 <juanpagab99@gmail.com>
2021-08-03 07:22:10 +0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const number = process.argv[2];
 if (!number) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(argument);
+  console.log(number);
 }
