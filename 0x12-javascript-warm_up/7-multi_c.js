36581208cad57f57afbd097726a7659b953a7574
Juanpagab99 <juanpagab99@gmail.com>
2021-08-03 08:21:19 +0700

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = process.argv[2];
+const phrase = 'C is fun';
+if (isNaN(x)) {
+	console.log("Missing number of occurrences");
+} else {
+	for (let i = 1; i <= x; i++) {
+		console.log(phrase);
+	}
+}
